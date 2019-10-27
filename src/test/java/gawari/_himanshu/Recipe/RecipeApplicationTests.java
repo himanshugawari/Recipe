674fe5b73e13fe9a03236034a49f919a0bc41e9c
Himanshu Gawari <himanshugawarigit@gmail.com>
2019-10-27 11:49:35 +0530

    Initial code base. closes #1

---
+++ src/test/java/gawari/_himanshu/Recipe/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package gawari._himanshu.Recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
