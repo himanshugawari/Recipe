0abefdcfb4f5dc58a1dd43078ae3ddc81fb1b52c
Himanshu Gawari <himanshugawarigit@gmail.com>
2019-11-02 21:34:02 +0530

    added data.sql

--- src/main/java/gawari/_himanshu/Recipe/controllers/IndexController.java
+++ src/main/java/gawari/_himanshu/Recipe/controllers/IndexController.java
@@ -1,13 +1,37 @@
 package gawari._himanshu.Recipe.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import gawari._himanshu.Recipe.model.Category;
+import gawari._himanshu.Recipe.model.UnitOfMeasure;
+import gawari._himanshu.Recipe.repositories.CategoryRepository;
+import gawari._himanshu.Recipe.repositories.UnitOfMeasureRepository;
 
 @Controller
 public class IndexController {
 
+	private final CategoryRepository categoryRepository;
+	private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+	@Autowired
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		super();
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
 	@RequestMapping({ "", "/", "/index" })
 	public String getIndexPage() {
+		Optional<Category> categoryOptional = categoryRepository.findByDescription("Fast Food");
+		Optional<UnitOfMeasure> unitOfMeasureRepositoryOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+		System.out.println("Cat Id is: " + categoryOptional.get().getId());
+		System.out.println("UOM Id is: " + unitOfMeasureRepositoryOptional.get().getId());
+
 		return "index";
 	}
 }
--- src/main/java/gawari/_himanshu/Recipe/repositories/UnitOfMeasureRepository.java
+++ src/main/java/gawari/_himanshu/Recipe/repositories/UnitOfMeasureRepository.java
@@ -1,9 +1,13 @@
 package gawari._himanshu.Recipe.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import gawari._himanshu.Recipe.model.UnitOfMeasure;
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+	Optional<UnitOfMeasure> findByDescription(String description);
 
 }
